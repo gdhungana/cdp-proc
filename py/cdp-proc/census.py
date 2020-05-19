795ba9a0467ea6a23c5b7c61bce15a84a699a978
gdhungana <gdhungana100@gmail.com>
2020-05-19 07:59:34 -0500

    function to evalueate economic averages at blk

--- py/cdp-proc/census.py
+++ py/cdp-proc/census.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def get_avg_commute(state,year,path):
+def get_blk_commute(state,year,path):
     filename=state+'Commute'+year[2:]+'.csv'
     comm=pd.read_csv(datapath+'/'+filename)
     comm.drop(axis=0,index=0,inplace=True)
@@ -13,6 +13,28 @@ def get_avg_commute(state,year,path):
     comm2['AvgCommute']=sum(comm2[com_fields[ii]]*com_fields_i[ii] for ii in np.arange(len(com_fields)))/comm2['CommuteN']
     return comm2[['BlkGrp','AvgCommute']]
 
-
+def get_blk_econ(state,year,path):
+    filename=state+'Econ'+year[2:]+'.csv'
+    econ=pd.read_csv(datapath+'/'+filename)
+    econ.drop(axis=0,index=0,inplace=True)
+    econ=econ.reset_index()
+    #- fields
+    LT40_fields=['LT10','LT15','LT20','LT25','LT30','LT35','LT40']
+    GT100_fields=['GT100','GT125','GT150','GT200']
+    GT125_fields=['GT125','GT150','GT200']
+    MidCI_fields=['LT45','LT50','LT60','LT55','LT75','LT100']
+    MidCI2_fields=['LT45','LT50','LT55','LT60','LT75','LT100','LT125']
+    #- convert the fields to float before calculation
+    fulllist=list(set(LT40_fields+GT100_fields+GT125_fields+MidCI_fields+MidCI2_fields))
+    print("Full list: ", fulllist)
+    othfields=['BlkGrp','TotHse']
+    econ=econ[fulllist+othfields].astype(int)
+    #- Get the metrics
+    econ['LT40p']=sum(econ[ii] for ii in LT40_fields)/econ['TotHse']
+    econ['GT100p']=sum(econ[ii] for ii in GT100_fields)/econ['TotHse']
+    econ['GT125p']=sum(econ[ii] for ii in GT125_fields)/econ['TotHse']
+    econ['MidCIsP']=sum(econ[ii] for ii in MidCI_fields)/econ['TotHse']
+    econ['MidCIsP2']=sum(econ[ii] for ii in MidCI2_fields)/econ['TotHse']
+    return econ[['BlkGrp','LT40p','GT100p','GT125p','MidCIsP']]
 
 
