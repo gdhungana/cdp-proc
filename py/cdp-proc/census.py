aa170de737e79825e153f5b35bee13d9ad720265
gdhungana <gdhungana100@gmail.com>
2020-05-19 08:15:40 -0500

    function to evalueate educational averages at blk

--- py/cdp-proc/census.py
+++ py/cdp-proc/census.py
@@ -11,7 +11,7 @@ def get_blk_commute(state,year,path):
     othfields=['BlkGrp','CommuteN']
     comm2=comm[othfields+com_fields].astype(int)
     comm2['AvgCommute']=sum(comm2[com_fields[ii]]*com_fields_i[ii] for ii in np.arange(len(com_fields)))/comm2['CommuteN']
-    return comm2[['BlkGrp','AvgCommute']]
+    return comm2[['BlkGrp','CommuteN','AvgCommute']]
 
 def get_blk_econ(state,year,path):
     filename=state+'Econ'+year[2:]+'.csv'
@@ -35,6 +35,18 @@ def get_blk_econ(state,year,path):
     econ['GT125p']=sum(econ[ii] for ii in GT125_fields)/econ['TotHse']
     econ['MidCIsP']=sum(econ[ii] for ii in MidCI_fields)/econ['TotHse']
     econ['MidCIsP2']=sum(econ[ii] for ii in MidCI2_fields)/econ['TotHse']
-    return econ[['BlkGrp','LT40p','GT100p','GT125p','MidCIsP']]
+    return econ[['BlkGrp','TotHse','LT40p','GT100p','GT125p','MidCIsP']]
+
+def get_blk_educ(state,year,path):
+    filename=state+'Educ'+year[2:]+'.csv'
+    educ=pd.read_csv(datapath+'/'+filename)
+    educ.drop(axis=0,index=0,inplace=True)
+    educ=educ.reset_index()
+    educ_fields=['BACH','GRAD','PROF','PHD']
+    othfields=['BlkGrp','POP25']
+    educ=educ[educ_fields+othfields].astype(int)
+    #- Get the stat.
+    educ['BachPerc']=sum(educ[ii] for ii in educ_fields)/educ['POP25']
+    return educ[['BlkGrp','POP25','BachPerc']]
 
 
